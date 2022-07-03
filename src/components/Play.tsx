f0dc6edfabb2765c05761abd31f8c3e057fc7d00
Eric Thomas <bluebird6922@gmail.com>
2022-07-03 04:34:16 +0300

    use ticket button

--- src/components/Play.tsx
+++ src/components/Play.tsx
@@ -1,14 +1,24 @@
 import * as React from "react";
 import {
   PlasmicPlay,
-  DefaultPlayProps
+  DefaultPlayProps,
 } from "./plasmic/online_course_or_book/PlasmicPlay";
 import { HTMLElementRefOf } from "@plasmicapp/react-web";
 
 export interface PlayProps extends DefaultPlayProps {}
 
 function Play_(props: PlayProps, ref: HTMLElementRefOf<"div">) {
-  return <PlasmicPlay root={{ ref }} {...props} />;
+  const handleUseTicket = () => {
+    console.log("handleUseTicket");
+  };
+  
+  return (
+    <PlasmicPlay
+      root={{ ref }}
+      {...props}
+      useTicket={{ onClick: () => handleUseTicket() }}
+    />
+  );
 }
 
 const Play = React.forwardRef(Play_);
