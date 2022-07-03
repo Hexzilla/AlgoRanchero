ecae92c6817803c4a390f369c02772abc03c16bd
Eric Thomas <bluebird6922@gmail.com>
2022-07-03 05:01:55 +0300

    react scripts

--- src/components/Play.tsx
+++ src/components/Play.tsx
@@ -4,14 +4,26 @@ import {
   DefaultPlayProps,
 } from "./plasmic/online_course_or_book/PlasmicPlay";
 import { HTMLElementRefOf } from "@plasmicapp/react-web";
+import useAssets from "../hooks/useAssets";
+import useWallet from "../hooks/useWallet";
 
 export interface PlayProps extends DefaultPlayProps {}
 
+const receiver = "H5ATUPW3P7P2XOGIY2EXA7YGOZLLYXWK44XVBSV5SAIMSKI35ZRN2EKTTA";;
+
 function Play_(props: PlayProps, ref: HTMLElementRefOf<"div">) {
-  const handleUseTicket = () => {
+  const { account } = useWallet();
+  const { sendAssets } = useAssets();
+
+  const handleUseTicket = async () => {
     console.log("handleUseTicket");
+    if (!account) {
+      return;
+    }
+    const response = await sendAssets(receiver);
+    console.log('response', response);
   };
-  
+
   return (
     <PlasmicPlay
       root={{ ref }}
